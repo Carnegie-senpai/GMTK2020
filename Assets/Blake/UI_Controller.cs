5ede5f97428f19e239846bcf467b2d85308cc8aa
Blake Myers <blaketoyo@live.com>
2020-07-10 19:19:32 -0700

    Added a work in progress menu

---
+++ Assets/Blake/UI_Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_Controller : MonoBehaviour
+{
+    public GameObject CreditsPanel;
+    public void QuitGame() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = (false);
+#else
+        Application.Quit();
+#endif
+
+    }
+
+    public void PlayGame() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ShowCredits()
+    {
+        if (!CreditsPanel.activeSelf)
+            CreditsPanel.SetActive(true);
+        else
+        {
+            CreditsPanel.SetActive(false);
+        }
+    }
+}
