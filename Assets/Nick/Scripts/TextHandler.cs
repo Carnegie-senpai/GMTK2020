d20ad86c5531e4ed8c83391693a17dee72375be0
Carnegie-senpai <sawyer3nick@gmail.com>
2020-07-11 01:48:22 -0700

    i gotta

---
+++ Assets/Nick/Scripts/TextHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextHandler : MonoBehaviour
+{
+    public GameObject prefab;
+    private ScrollingText scrolltext;
+    private string texts;
+    private GameObject instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+        texts = "Hello World!\\nThis is a test.";
+        instance = Instantiate(prefab,gameObject.transform);
+        scrolltext = instance.GetComponent<ScrollingText>();
+        scrolltext.message = texts;
+        //scrolltext.count = 0;
+    }
+}
