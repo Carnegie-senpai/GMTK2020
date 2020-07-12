cdf40017f35f66357cc0e2f752270189913c66e4
Blake Myers <blaketoyo@live.com>
2020-07-12 03:31:32 -0700

    added end screen

--- Assets/Checkpoint_Controller.cs
+++ Assets/Checkpoint_Controller.cs
@@ -7,9 +7,45 @@ public class Checkpoint_Controller : MonoBehaviour
     // Start is called before the first frame update
     public int Moveset = 0;
     public GameObject player;
+    public bool isEnd = false;
+    public GameObject EndPanel;
+    private float time;
+    private bool Triggered = false;
+    //public GameObject endButton;
+    int i = 0;
     void Start()
     {
        player = GameObject.Find("Player");
+        EndPanel = GameObject.Find("EndPanel");
+      //  endButton = GameObject.Find("endButton");
+    }
+
+    private void Update()
+    {
+        if (isEnd && Triggered)
+        {
+            if(i <= 9)
+            {
+                if (Time.time - time > 0.5)
+                {
+                    time = Time.time;
+                    if (!EndPanel.transform.GetChild(i).gameObject.activeSelf)
+                    {
+                        EndPanel.transform.GetChild(i).gameObject.SetActive(true);
+                    }
+                    i++;
+                }
+            }
+            if (i == 10 && Time.time - time > 5.0) {
+                EndPanel.transform.GetChild(i).gameObject.SetActive(true);
+            
+            }
+
+        }
+    }
+
+    private void Endgame() { 
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -19,6 +55,10 @@ public class Checkpoint_Controller : MonoBehaviour
             Debug.Log("Respawn set to " + collision.gameObject.transform.position);
             if (player.GetComponent<PlayerController>().currentInputSet < Moveset)
                 player.GetComponent<PlayerController>().currentInputSet = Moveset;
+            if (isEnd) {
+                Triggered = true;
+            }
+            
         }
     }
 }
